2ed4f723a798edb9975526bf8d076661ccf27cac
factor-4 <snk.sojib@gmail.com>
2024-09-06 22:18:29 +0300

    mini component added

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,17 @@ builder.Services.AddDbContext<StoreContext> (opt => {
    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +32,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt => 
+{
+opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.MapControllers();
 
