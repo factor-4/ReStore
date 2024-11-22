a9772e58fe8c1f45acb657b0ada32c321245ea2d
factor-4 <snk.sojib@gmail.com>
2024-11-22 19:51:30 +0200

    axios interceptor added

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -7,11 +7,11 @@ export default function AboutPage() {
             <Typography gutterBottom variant="h2">Errors for testing purposes</Typography>
 
             <ButtonGroup fullWidth>
-            <Button variant='contained' onClick={() => agent.TestErrors.get400Error()}>Test 400 error</Button>
-            <Button variant='contained' onClick={() => agent.TestErrors.get401Error()}>Test 401 error</Button>
-            <Button variant='contained' onClick={() => agent.TestErrors.get404Error()}>Test 404 error</Button>
-            <Button variant='contained' onClick={() => agent.TestErrors.get500Error()}>Test 500 error</Button>
-            <Button variant='contained' onClick={() => agent.TestErrors.getValidationError()}>getValidationError error</Button>
+            <Button variant='contained' onClick={() => agent.TestErrors.get400Error().catch(error => console.log(error))}>Test 400 error</Button>
+            <Button variant='contained' onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))}>Test 401 error</Button>
+            <Button variant='contained' onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))}>Test 404 error</Button>
+            <Button variant='contained' onClick={() => agent.TestErrors.get500Error().catch(error => console.log(error))}>Test 500 error</Button>
+            <Button variant='contained' onClick={() => agent.TestErrors.getValidationError().catch(error => console.log(error))}>getValidationError error</Button>
             </ButtonGroup>
         </Container>
     )
